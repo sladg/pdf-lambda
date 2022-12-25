b0de27568f805f309a41f3a1e97dac0571fbe35f
Jan Soukup <jan@ssoukup.com>
2022-12-25 22:08:07 +0100

    🐛 fix(script): echoing additional things for debugging

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,9 @@
 set -e
 echo "Deploying PDF Lambda..."
 
+echo "The script you are running has basename $(basename -- "$0"), dirname $(dirname -- "$0")"
+echo "The present working directory is $(pwd)"
+
 CURRENT_DIR=$(pwd)
 
 # Set working environment to folder of this command.
